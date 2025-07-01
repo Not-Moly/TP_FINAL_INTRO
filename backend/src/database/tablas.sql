b5ea0a4221f57c471abc618a0efb772e59d31b61
Manuel <mmartineza@fi.uba.ar>
2025-07-01 18:00:41 -0300

    Realizo cambios en ejemplos.sql y tablas.sql

--- backend/src/database/tablas.sql
+++ backend/src/database/tablas.sql
@@ -1,5 +1,5 @@
 create table developers (
-	id_developer serial primary key,
+	id serial primary key,
 	name varchar(300),
 	foundation_year int,
 	cant_games int,
@@ -8,27 +8,27 @@ create table developers (
 )
 
 create table games (
-	id_game serial primary key,
+	id serial primary key,
 	title varchar(100),
 	release_year int,
 	gamemode varchar(100),
 	gender varchar(300),
 	perspective varchar(300),
-	img varchar(255),
-	frachise varchar(100)
-	developers int references developers (id_developer)
+	image varchar(255),
+	frachise varchar(100),
+	id_developer int references developers (id)
 );
 
 create table characters (
-	id_character serial primary key,
+	id serial primary key,
 	char_name varchar(100),
 	franchise varchar(100),
-	img varchar(255),
+	image varchar(255),
 	sex varchar(50),
 	species varchar(50),
 	description varchar(500),
 	main_skill varchar(100),
-	game int references games (id_game)
+	id_game int references games (id)
 );
 
 
