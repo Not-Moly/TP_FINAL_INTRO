cf34b22c83757e2984d75282a1b4f55ce50f7f5c
Federico Molina <federico.molina004hello@gmail.com>
2025-07-01 20:05:07 +0000

    Integro primeras funciones de 'playle.js' a 'api.js'

--- backend/src/api.js
+++ backend/src/api.js
@@ -5,6 +5,43 @@ app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
 
+const {
+    getAllGames,
+} = require('./database/playle');
+
+// 5 Endpoints per entity!!
+
+//#region GAMES
+
+//GET________________________________________________________
+// ONE from 'games' table by id
+app.get('api/games/:id',(req,res)=>{
+    // let game = games[req.params.num];
+    res.json({status: 'OK'});
+});
+// Get ALL data from 'games' table
+app.get('/api/games', async (req, res) =>{
+    let games = await getAllGames();
+    res.json(games);
+});
+
+//POST________________________________________________________
+app.post('/api/games',(req,res)=>{
+
+});
+
+//DELETE________________________________________________________
+app.delete('api/games/:id',(req,res)=>{
+
+});
+
+//PUT________________________________________________________
+app.put('api/games',(req,res) => {
+
+});
+
+//#endregion
+
 app.listen(PORT, () => {
     console.log("Server LIstening on PORT:",PORT);
-})
+});
