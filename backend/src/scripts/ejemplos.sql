af455d5462700b6a2b5e99792268f273f42ffdd3
Manuel <mmartineza@fi.uba.ar>
2025-07-01 16:47:44 -0300

    Realizo cambios en los archivos de la carpeta scripts y la posiciono en src

---
+++ backend/src/scripts/ejemplos.sql
@@ -0,0 +1,10 @@
+INSERT INTO developers (name,foundation_year,cant_games,origin_country,entity_type)
+VALUES ('Mojang',2009,22,'Suecia','Subsidiary')
+
+INSERT INTO games (title,developer,release_year,gamemode,gender,perspective,image,frachise,developers) 
+VALUES ('Minecraft',2009,'Un Jugador,Multijugador,Cooperativo','Survival,Exploracion,Sandbox','Primera Persona,Tercera Persona',
+'https://image.api.playstation.com/vulcan/ap/rnd/202407/0401/670c294ded3baf4fa11068db2ec6758c63f7daeb266a35a1.png','Minecraft',1)
+
+INSERT INTO characters (char_name,franchise,image,sex,species,description,main_skill,game)
+VALUES ('Steve','Minecraft','https://upload.wikimedia.org/wikipedia/en/e/e7/Steve_%28Minecraft%29.png','Humano',
+'Personaje jugable predeterminado del juego, lleva remera celeste, pantalones azules y tiene una fuerza sobre humana','Superfuerza',1)
--- backend/src/scripts/tablas.sql
+++ backend/src/scripts/tablas.sql
@@ -1,16 +1,25 @@
-create table game (
+create table developers (
+	id_developer serial primary key,
+	name varchar(300),
+	foundation_year int,
+	cant_games int,
+	origin_country varchar(50),
+	entity_type varchar(50)
+)
+
+create table games (
 	id_game serial primary key,
 	title varchar(100),
-	developer varchar(300),
 	release_year int,
 	gamemode varchar(100),
 	gender varchar(300),
 	perspective varchar(300),
 	img varchar(255),
 	frachise varchar(100)
+	developers int references developers (id_developer)
 );
 
-create table character (
+create table characters (
 	id_character serial primary key,
 	char_name varchar(100),
 	franchise varchar(100),
@@ -18,15 +27,9 @@ create table character (
 	sex varchar(50),
 	species varchar(50),
 	description varchar(500),
-	main_skill varchar(100)
+	main_skill varchar(100),
+	game int references games (id_game)
 );
 
-create table developer (
-	id_developer serial primary key,
-	name varchar(300),
-	foundation_year int,
-	cant_games int,
-	origin_country varchar(50),
-	entity_tipe varchar(50)
-)
+
 
