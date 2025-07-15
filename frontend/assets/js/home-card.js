79b4bdc0fd2fc4e67c469852a8d8501c73de734c
Federico Molina <federico.molina004hello@gmail.com>
2025-07-15 19:57:18 +0000

    Agrego efecto escritura del mensaje principal y Cambio pantalla de carga

--- frontend/assets/js/home-card.js
+++ frontend/assets/js/home-card.js
@@ -1,8 +1,23 @@
 // Get the Home card
 const homeCard = document.querySelector('.home-card');
 
-// Add neon effect flicker once the screen has loaded
+// Once the screen has loaded
 document.addEventListener("screenLoaded", () => {
-    console.log("loaded");
+    // Adds neon effect flicker 
     homeCard.classList.add('flicker-box-start');
+
+    // Adds type effect
+    let writeInText = document.querySelector(".write-in");
+    const fullText = writeInText.textContent;
+    writeInText.textContent = '';
+    let i = 0;
+    const time_between_letters = 35;
+    function type() {
+        if (i < fullText.length) {
+            writeInText.textContent += fullText.charAt(i);
+            i++;
+            setTimeout(type, time_between_letters);
+        }
+    }
+    type();
 });
