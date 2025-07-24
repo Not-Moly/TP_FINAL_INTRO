1db376888665f8440e264e268dc0359fc88405bd
Manuel <mmartineza@fi.uba.ar>
2025-07-24 00:44:57 -0300

    Arreglo error en declaracion de las variables iniciales de los archivos db

--- backend/src/database/games_db.js
+++ backend/src/database/games_db.js
@@ -1,7 +1,7 @@
 const { Pool } = require('pg');
 
 const dbClient = new Pool({
-  host: process.env.DB_HOST || 'localhost',
+  host: process.env.DB_HOST || 'postgres',
   port: process.env.DB_PORT || 5432,
   user: process.env.DB_USER || 'postgres',
   password: process.env.DB_PASSWORD || 'postgres',
--- backend/src/database/sagas_db.js
+++ backend/src/database/sagas_db.js
@@ -1,13 +1,12 @@
 const { Pool } = require('pg');
 
 const dbClient = new Pool({
-  host: process.env.DB_HOST || 'localhost',
+  host: process.env.DB_HOST || 'postgres',
   port: process.env.DB_PORT || 5432,
   user: process.env.DB_USER || 'postgres',
   password: process.env.DB_PASSWORD || 'postgres',
   database: process.env.DB_NAME || 'playle',
 });
-
 // ╔═══━━━━━━━━━━━━─── • ───━━━━━━━━━━━━═══╗
 //                POST (CREATE)
 // ╚═══━━━━━━━━━━━━─── • ───━━━━━━━━━━━━═══╝
