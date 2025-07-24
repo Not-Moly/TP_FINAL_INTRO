d1a7bc803ce44affaf0721f4246b1f3806a6523c
Federico Molina <federico.molina004hello@gmail.com>
2025-07-24 01:26:23 +0000

    Arreglos para docker compose

--- backend/src/database/characters_db.js
+++ backend/src/database/characters_db.js
@@ -1,6 +1,6 @@
-const { Client } = require('pg');
+const { Pool } = require('pg');
 
-const dbClient = new Client({
+const dbClient = new Pool({
   host: process.env.DB_HOST || 'localhost',
   port: process.env.DB_PORT || 5432,
   user: process.env.DB_USER || 'postgres',
--- backend/src/database/games_db.js
+++ backend/src/database/games_db.js
@@ -1,14 +1,12 @@
-const { Client } = require('pg');
+const { Pool } = require('pg');
 
-const dbClient = new Client({
+const dbClient = new Pool({
   host: process.env.DB_HOST || 'localhost',
   port: process.env.DB_PORT || 5432,
   user: process.env.DB_USER || 'postgres',
   password: process.env.DB_PASSWORD || 'postgres',
   database: process.env.DB_NAME || 'playle',
 });
-
-dbClient.connect();
 
 // ╔═══━━━━━━━━━━━━─── • ───━━━━━━━━━━━━═══╗
 //                POST (CREATE)
